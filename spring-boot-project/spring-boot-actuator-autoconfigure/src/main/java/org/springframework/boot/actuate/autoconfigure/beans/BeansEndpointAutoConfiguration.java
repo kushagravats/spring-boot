2cb069b6f6984a86cfd399c4c51b4fa11a4e7fcc
Kushagra Vats <44410587+kushagravats@users.noreply.github.com>
2024-05-07 00:35:22 +0530

    Update BeansEndpointAutoConfiguration.java

--- spring-boot-project/spring-boot-actuator-autoconfigure/src/main/java/org/springframework/boot/actuate/autoconfigure/beans/BeansEndpointAutoConfiguration.java
+++ spring-boot-project/spring-boot-actuator-autoconfigure/src/main/java/org/springframework/boot/actuate/autoconfigure/beans/BeansEndpointAutoConfiguration.java
@@ -1,43 +1,28 @@
-/*
- * Copyright 2012-2022 the original author or authors.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      https://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package org.springframework.boot.actuate.autoconfigure.beans;
 
-import org.springframework.boot.actuate.autoconfigure.endpoint.condition.ConditionalOnAvailableEndpoint;
 import org.springframework.boot.actuate.beans.BeansEndpoint;
 import org.springframework.boot.autoconfigure.AutoConfiguration;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.actuate.autoconfigure.endpoint.condition.ConditionalOnAvailableEndpoint;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Bean;
 
 /**
- * {@link EnableAutoConfiguration Auto-configuration} for the {@link BeansEndpoint}.
+ * {@link AutoConfiguration} for the {@link BeansEndpoint}.
+ * This configuration class ensures that the BeansEndpoint is only configured when
+ * it is available and not already defined in the application context.
  *
  * @author Phillip Webb
  * @since 2.0.0
  */
-@AutoConfiguration
+@AutoConfiguration // This replaces @EnableAutoConfiguration if it was used
 @ConditionalOnAvailableEndpoint(endpoint = BeansEndpoint.class)
 public class BeansEndpointAutoConfiguration {
 
-	@Bean
-	@ConditionalOnMissingBean
-	public BeansEndpoint beansEndpoint(ConfigurableApplicationContext applicationContext) {
-		return new BeansEndpoint(applicationContext);
-	}
+    @Bean
+    @ConditionalOnMissingBean
+    public BeansEndpoint beansEndpoint(ConfigurableApplicationContext applicationContext) {
+        return new BeansEndpoint(applicationContext);
+    }
 
 }
