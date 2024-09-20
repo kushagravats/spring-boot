d71d80cae4f9b8c023f9874dfdeef48f5eaa845e
Kushagra Vats <44410587+kushagravats@users.noreply.github.com>
2024-09-20 21:08:35 +0530

    Update basic-launch.sh

--- spring-boot-tests/spring-boot-integration-tests/spring-boot-launch-script-tests/src/intTest/resources/scripts/init.d/basic-launch.sh
+++ spring-boot-tests/spring-boot-integration-tests/spring-boot-launch-script-tests/src/intTest/resources/scripts/init.d/basic-launch.sh
@@ -1,3 +1,18 @@
+#!/bin/bash
+
+git checkout 2.3.x
+
+git merge 2.2.x
+
+if [ $? -ne 0 ]; then
+    echo "Merge conflicts detected. Please resolve them and then run:"
+    echo "git add <file_with_conflicts>"
+    echo "git commit"
+    exit 1
+fi
+
+git push origin 2.3.x
+
 source ./test-functions.sh
 install_service
 start_service
